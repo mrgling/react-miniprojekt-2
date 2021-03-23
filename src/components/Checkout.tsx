87bb1b9106eac6b9c8e3aac99d0c8cb5d33e7196
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-03-23 10:39:33 +0100

    shipping work

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -11,7 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import CustomerForm, { Customer } from './CustomerForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
-import Shipping from './Shipping';
+import Shipping, { ShippingInfo } from './Shipping';
 
 export function Copyright() {
   return (
@@ -84,7 +84,8 @@ export default function Checkout() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const [addressInfo, setAddressInfo] = React.useState('address');
-  const [customer, setCustomer] = React.useState<Customer>({})
+  const [customer, setCustomer] = React.useState<Customer>({  firstName: '', lastName: '', address: '', zip: '',  city: '', phoneNumber: '', email: ''})
+  const [shippingInfo, setShippingInfo] = React.useState<ShippingInfo>({  agent: '', shippingPrice: '', shippingDate: ''})
   
   const handleNext = () => {
     setActiveStep(activeStep + 1);
@@ -103,7 +104,7 @@ export default function Checkout() {
       case 0:
         return <CustomerForm handleNext={handleNext} customer={customer} onCustomerChange={setCustomer} />;
       case 1:
-        return <Shipping handleNext={handleNext} handleBack={handleBack} />;
+        return <Shipping handleNext={handleNext} handleBack={handleBack} shippingInfo={shippingInfo} onShippingChange={setShippingInfo}/>;
       case 2:
           return <PaymentForm handleNext={handleNext} handleBack={handleBack} />;
       case 3:
--- src/components/Shipping.tsx
+++ src/components/Shipping.tsx
@@ -9,13 +9,22 @@ import { Button } from '@material-ui/core';
 interface Props {
   handleNext: () => void;
   handleBack: () => void;
+  shippingInfo: ShippingInfo;
+  onShippingChange: (shippingInfo: ShippingInfo) => void;
 }
 
-  export default function Shipping(props: Props) {
-  const [value, setValue] = React.useState('postnord');
+export interface ShippingInfo {
+  agent: string;
+  shippingPrice: string;
+  shippingDate: string;
+}
 
+export default function Shipping(props: Props) {
+  const [value, setValue] = React.useState('postnord');
+  
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue((event.target as HTMLInputElement).value);
+    console.log(event.target.value);
   };
 
   return (
@@ -25,11 +34,11 @@ interface Props {
       <FormLabel component="legend">Välj fraksätt</FormLabel>
       <RadioGroup aria-label="shipping" name="shipping1" value={value} onChange={handleChange}>
         <FormControlLabel value="postnord" control={<Radio color="primary" />} label="Postnord" />
-        <span> Fraktkostnad: 200 kr Leveranstid: 1-2 vardagar</span>
-        <FormControlLabel value="dhl" control={<Radio color="primary" />} label="DHL" />
-        <span> Fraktkostnad: 129 kr Leveranstid: 3-4 vardagar</span>
+        <span> Fraktkostnad: 49 kr Leveranstid: 5 vardagar</span>
         <FormControlLabel value="ups" control={<Radio color="primary" />} label="UPS" />
-        <span> Fraktkostnad: 75 kr Leveranstid: 6-7 vardagar</span>
+        <span> Fraktkostnad: 89 kr Leveranstid: 2 vardagar</span>
+        <FormControlLabel value="dhl" control={<Radio color="primary" />} label="DHL" />
+        <span> Fraktkostnad: 149 kr Leveranstid: 1 vardagar</span>
       </RadioGroup>
       <div >
           <Button
@@ -49,6 +58,7 @@ interface Props {
         </div>
     </FormControl>
     
-  );
-}
+    );
+  }
+  
 
